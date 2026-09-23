dc60e3f010191b97870ae92789147b2e22fc2751
marcoABZ <76532415+marcoABZ@users.noreply.github.com>
2026-09-23 20:44:08 -0300

    Time: 155 ms (5.92%) | Memory: 24.4 MB (57.59%) - LeetSync

---
+++ 334-increasing-triplet-subsequence/increasing-triplet-subsequence.py
@@ -0,0 +1,33 @@
+class Solution(object):
+    def increasingTriplet(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        if len(nums) < 3:
+            return False
+
+        i, j, k = 0, 1, None
+        while j < len(nums) and (k == None or k < len(nums)):
+            if not k:
+                if nums[j] < nums[i]:
+                    i = j
+                    j += 1
+                elif nums[j] > nums[i]:
+                    k = j+1
+                else:
+                    j += 1
+            else:
+                if nums[k] < nums[j] and nums[k] < nums[i]:
+                    if self.increasingTriplet(nums[k:]):
+                        return True
+                    k += 1
+                elif nums[k] < nums[j] and nums[k] > nums[i]:
+                    j = k
+                    k += 1
+                elif nums[k] > nums[j]:
+                    return True
+                else:
+                    k += 1
+        
+        return False
