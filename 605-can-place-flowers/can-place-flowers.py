46078f8b2b6134bbbb37cced0acb8683d7878eda
marcoABZ <76532415+marcoABZ@users.noreply.github.com>
2026-09-23 12:20:31 -0300

    Time: 4 ms (94.35%) | Memory: 13 MB (51.05%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.py
+++ 605-can-place-flowers/can-place-flowers.py
@@ -12,22 +12,20 @@ class Solution(object):
             return flowerbed[0] == 0 and n <= 1
     
         plants = 0
-
-        left = flowerbed[:]
-        if left[0] == 0 and left[1] == 0:
-            left[0] = 1
+        if flowerbed[0] == 0 and flowerbed[1] == 0:
+            flowerbed[0] = 1
             plants += 1
 
         for i in range(1, len(flowerbed) - 1):
-            if left[i] == 1:
+            if flowerbed[i] == 1:
                 continue
     
-            if left[i-1] == 0 and left[i+1] == 0:
-                left[i] = 1
+            if flowerbed[i-1] == 0 and flowerbed[i+1] == 0:
+                flowerbed[i] = 1
                 plants += 1
         
-        if left[-1] == 0 and left[-2] == 0:
-            left[-1] = 1
+        if flowerbed[-1] == 0 and flowerbed[-2] == 0:
+            flowerbed[-1] = 1
             plants += 1
         
         return plants >= n
